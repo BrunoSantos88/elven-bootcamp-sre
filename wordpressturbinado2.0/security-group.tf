fbb33989d196b63efbe44e35010df691bfcad514
BrunoSantos88 <brunosantosc1@gmail.com>
2025-05-11 15:45:07 -0300

    insfraiscode

--- wordpressturbinado2.0/security-group.tf
+++ wordpressturbinado2.0/security-group.tf
@@ -1,51 +1,117 @@
-resource "aws_security_group" "allow_tls" {
-  name        = "Firewall-wordpressturbinado"
-  description = "Allow TLS inbound traffic and all outbound traffic"
+# Security group EC2
+resource "aws_security_group" "sc-ec2-wordpress" {
+  name        = "pemitir ssh, http, no"
+  description = "Allow SSH, HTTP, and Node Exporter inbound traffic"
   vpc_id      = aws_vpc.srewordpressturbinado_publica.id
 
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Node Exporter"
+    from_port   = 9100
+    to_port     = 9100
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
   tags = {
-    Name = "secgroup-wordpressturbinado"
+    Name = "allow_ssh_http_node"
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "https" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = aws_vpc.srewordpressturbinado_publica.cidr_block
-  from_port         = 443
-  ip_protocol       = "tcp"
-  to_port           = 443
+# Security group RDS
+resource "aws_security_group" "allow_rds" {
+  name        = "allow_rds"
+  description = "Allow MySQL traffic from EC2 instances"
+  vpc_id      = aws_vpc.srewordpressturbinado_publica.id
+
+  ingress {
+    description = "MySQL"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_rds"
+  }
 }
 
+# Security group monitor (Prometheus + Grafana)
+resource "aws_security_group" "allow_monitor" {
+  name        = "allow_monitor"
+  description = "Allow monitoring traffic (Prometheus, Grafana)"
+  vpc_id      = aws_vpc.srewordpressturbinado_publica.id
 
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-resource "aws_vpc_security_group_ingress_rule" "http" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = aws_vpc.srewordpressturbinado_publica.cidr_block
-  from_port         = 80
-  ip_protocol       = "tcp"
-  to_port           = 80
-}
+  ingress {
+    description = "Grafana"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
+  ingress {
+    description = "Prometheus"
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-resource "aws_vpc_security_group_ingress_rule" "rds" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = aws_vpc.srewordpressturbinado_publica.cidr_block
-  from_port         = 3306
-  ip_protocol       = "tcp"
-  to_port           = 3306
-}
-
-resource "aws_vpc_security_group_ingress_rule" "ssh" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = aws_vpc.srewordpressturbinado_publica.cidr_block
-  from_port         = 22
-  ip_protocol       = "tcp"
-  to_port           = 22
-}
-
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
+  tags = {
+    Name = "allow_monitor"
+  }
 }
